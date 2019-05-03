a693a7ca901dcc1e76b100998a5f2d06e1f9a2eb
WILMER LEONEL ARTEAGA GAITAN <artewilm@colpatria.com>
2019-05-03 18:10:14 -0500

    Comparing Arrays in JUnit Tests

---
+++ JunitCourse/src/test/java/com/killerwilmer/junit/helper/ArraysCompareTest.java
@@ -0,0 +1,23 @@
+package com.killerwilmer.junit.helper;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class ArraysCompareTest {
+
+	// Array.sort
+	
+	@Test
+	public void testArraySort_RandomArray() {
+		int[] actualNumbers = {12, 3, 4, 1};
+		int[] expectedNumbers = {1, 3, 4, 12};
+		
+		Arrays.sort(actualNumbers);
+		
+		assertArrayEquals(expectedNumbers, actualNumbers);
+	}
+
+}
