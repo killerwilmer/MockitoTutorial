a8a13ebd5c4027ca40028514655885771fa0444e
Wilmer Arteaga <killerwilmer@gmail.com>
2019-05-07 22:41:01 -0500

    Finished first test mockito code

--- MockitoCourse/src/test/java/com/killerwilmer/business/TodoBusinessImplMockTest.java
+++ MockitoCourse/src/test/java/com/killerwilmer/business/TodoBusinessImplMockTest.java
@@ -2,7 +2,9 @@ package com.killerwilmer.business;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,7 +13,7 @@ import com.killerwilmer.data.api.TodoService;
 import com.killerwilmer.data.api.TodoServiceStub;
 
 public class TodoBusinessImplMockTest {
-	
+
 	// What is mocking?
 	// Mocking is creating objects that simulate the behavior of real objects.
 	// Unlike stubs, mocks can be dynamically created from code - at runtime.
@@ -20,14 +22,36 @@ public class TodoBusinessImplMockTest {
 
 	@Test
 	public void testRetrieveTodosRelatedToSpring_usingAMock() {
-		
+
 		TodoService todoServiceMock = mock(TodoService.class);
+
+		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring Boot", "Learn to Dance");
+
+		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
+
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-		
+
 		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-		
-		assertEquals(2,  filteredTodos.size());
-		
+
+		assertEquals(2, filteredTodos.size());
+
+	}
+
+	@Test
+	public void testRetrieveTodosRelatedToSpring_withEmptyList() {
+
+		TodoService todoServiceMock = mock(TodoService.class);
+
+		List<String> todos = Arrays.asList();
+
+		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
+
+		assertEquals(0, filteredTodos.size());
+
 	}
 
 }
