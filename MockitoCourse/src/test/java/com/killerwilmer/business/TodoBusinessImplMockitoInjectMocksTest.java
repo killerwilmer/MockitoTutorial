0b01e6140645c6bcaaecd837c4dda4e899f5ca2c
WILMER LEONEL ARTEAGA GAITAN <artewilm@colpatria.com>
2019-05-09 13:23:50 -0500

    JUnit Rules. Using MockitoJUnit.rule() instead of @RunWith(MockitoJUnitRunner.class).

--- MockitoCourse/src/test/java/com/killerwilmer/business/TodoBusinessImplMockitoInjectMocksTest.java
+++ MockitoCourse/src/test/java/com/killerwilmer/business/TodoBusinessImplMockitoInjectMocksTest.java
@@ -4,24 +4,25 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import com.killerwilmer.data.api.TodoService;
 
-@RunWith(MockitoJUnitRunner.class)
 public class TodoBusinessImplMockitoInjectMocksTest {
+	
+	@Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 	
 	@Mock
 	TodoService todoServiceMock;
